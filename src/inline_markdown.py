2260a06370510b6a4d7b4f4dc49e8837326b7238
archquent <quent.devacct@gmail.com>
2025-03-01 15:26:56 -0600

    fixed italic delimiter issue

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -83,7 +83,7 @@ def split_nodes_link(old_nodes):
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
